3313d275a55bbb1a1f7424e5dcca53d41988bbf8
keith duncan <duncanclanof4@gmail.com>
2023-08-08 11:38:55 -0400

    New Database design with v5 with hypoallergenic

---
+++ dotnet/database/DB_v5_with_hypoallergenic.sql
@@ -0,0 +1,270 @@
+USE [master]
+drop database if exists [final_capstone]
+GO
+/****** Object:  Database [final_capstone]    Script Date: 8/8/2023 11:37:08 AM ******/
+CREATE DATABASE [final_capstone]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'final_capstone', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\final_capstone.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'final_capstone_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\final_capstone_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [final_capstone] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [final_capstone].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [final_capstone] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [final_capstone] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [final_capstone] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [final_capstone] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [final_capstone] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [final_capstone] SET AUTO_CLOSE ON 
+GO
+ALTER DATABASE [final_capstone] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [final_capstone] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [final_capstone] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [final_capstone] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [final_capstone] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [final_capstone] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [final_capstone] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [final_capstone] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [final_capstone] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [final_capstone] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [final_capstone] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [final_capstone] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [final_capstone] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [final_capstone] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [final_capstone] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [final_capstone] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [final_capstone] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [final_capstone] SET  MULTI_USER 
+GO
+ALTER DATABASE [final_capstone] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [final_capstone] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [final_capstone] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [final_capstone] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [final_capstone] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [final_capstone] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [final_capstone] SET QUERY_STORE = OFF
+GO
+USE [final_capstone]
+GO
+/****** Object:  Table [dbo].[addresses]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[addresses](
+	[address_id] [int] NOT NULL,
+	[street] [varchar](50) NOT NULL,
+	[city] [varchar](50) NOT NULL,
+	[state_abr] [varchar](2) NOT NULL,
+	[zip] [int] NOT NULL,
+ CONSTRAINT [PK_addresses] PRIMARY KEY CLUSTERED 
+(
+	[address_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[attributes]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[attributes](
+	[attribute_id] [int] IDENTITY(1,1) NOT NULL,
+	[spayed_neutered] [bit] NOT NULL,
+	[house_trained] [bit] NOT NULL,
+	[declawed] [bit] NOT NULL,
+	[special_needs] [bit] NOT NULL,
+	[shots_current] [bit] NOT NULL,
+ CONSTRAINT [PK_attributes] PRIMARY KEY CLUSTERED 
+(
+	[attribute_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[environments]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[environments](
+	[environment_id] [int] IDENTITY(1,1) NOT NULL,
+	[children] [bit] NOT NULL,
+	[dogs] [bit] NOT NULL,
+	[cats] [bit] NOT NULL,
+	[other_animals] [bit] NOT NULL,
+	[indoor_only] [bit] NOT NULL,
+ CONSTRAINT [PK_environment] PRIMARY KEY CLUSTERED 
+(
+	[environment_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[pets]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[pets](
+	[pet_id] [int] IDENTITY(1,1) NOT NULL,
+	[type] [nvarchar](50) NOT NULL,
+	[species] [nvarchar](50) NOT NULL,
+	[color] [varchar](20) NOT NULL,
+	[age] [int] NULL,
+	[attribute_id] [int] NOT NULL,
+	[environment_id] [int] NOT NULL,
+	[tag_id] [int] NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[description] [nvarchar](200) NULL,
+	[location_zip] [int] NOT NULL,
+	[user_id] [int] NOT NULL,
+	[photo_id] [int] NULL,
+	[is_adopted] [bit] NOT NULL,
+	[adopter_id] [int] NULL,
+ CONSTRAINT [PK_Pet] PRIMARY KEY CLUSTERED 
+(
+	[pet_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[photos]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[photos](
+	[photo_id] [int] IDENTITY(1,1) NOT NULL,
+	[photo_url] [nvarchar](200) NOT NULL,
+	[pet_id] [int] NOT NULL,
+ CONSTRAINT [PK_Photos] PRIMARY KEY CLUSTERED 
+(
+	[photo_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tags]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tags](
+	[tag_id] [int] IDENTITY(1,1) NOT NULL,
+	[playful] [bit] NOT NULL,
+	[needs_exercise] [bit] NOT NULL,
+	[cute] [bit] NOT NULL,
+	[affectionate] [bit] NOT NULL,
+	[large] [bit] NOT NULL,
+	[intelligent] [bit] NOT NULL,
+	[happy] [bit] NOT NULL,
+	[short_haired] [bit] NOT NULL,
+	[shedder] [bit] NOT NULL,
+	[shy] [bit] NOT NULL,
+	[faithful] [bit] NOT NULL,
+	[leash_trained] [bit] NOT NULL,
+	[hypoallergenic] [bit] NOT NULL,
+ CONSTRAINT [PK_tags] PRIMARY KEY CLUSTERED 
+(
+	[tag_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[users]    Script Date: 8/8/2023 11:37:08 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[user_id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](50) NOT NULL,
+	[password_hash] [varchar](200) NOT NULL,
+	[salt] [varchar](200) NOT NULL,
+	[user_role] [varchar](50) NOT NULL,
+	[user_location] [int] NULL,
+	[app_status] [varchar](10) NULL,
+	[is_not_active] [bit] NOT NULL,
+	[address_id] [int] NULL,
+	[email] [varchar](50) NULL,
+	[is_adopter] [bit] NULL,
+ CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ_UserName] UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_attributes] FOREIGN KEY([attribute_id])
+REFERENCES [dbo].[attributes] ([attribute_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_attributes]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_environments] FOREIGN KEY([environment_id])
+REFERENCES [dbo].[environments] ([environment_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_environments]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_Photos] FOREIGN KEY([pet_id])
+REFERENCES [dbo].[photos] ([photo_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_Photos]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_tags] FOREIGN KEY([tag_id])
+REFERENCES [dbo].[tags] ([tag_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_tags]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_users] FOREIGN KEY([user_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_users]
+GO
+ALTER TABLE [dbo].[pets]  WITH CHECK ADD  CONSTRAINT [FK_pets_users1] FOREIGN KEY([pet_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[pets] CHECK CONSTRAINT [FK_pets_users1]
+GO
+ALTER TABLE [dbo].[users]  WITH CHECK ADD  CONSTRAINT [FK_users_addresses] FOREIGN KEY([address_id])
+REFERENCES [dbo].[addresses] ([address_id])
+GO
+ALTER TABLE [dbo].[users] CHECK CONSTRAINT [FK_users_addresses]
+GO
+USE [master]
+GO
+ALTER DATABASE [final_capstone] SET  READ_WRITE 
+GO
