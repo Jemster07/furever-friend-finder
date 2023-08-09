3e3aaf751da352e9f72c00d8d2b58347d2a460c2
Riley Black <rstoneblack@gmail.com>
2023-08-09 13:54:01 -0400

    More front end work. Awaiting Bryce's input

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,21 +1,66 @@
 <template>
-  <nav class="navbar">
-    <div class="navbar-brand">
-      <a class="navbar-item">
-        <img src="../img/FFF-Banner-Transparent.png" alt="site-logo" style="max-height: 100px" />
-      </a>
-    </div>
+  <div class="has-text-centered">
+    <nav class="navbar has-shadow">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img
+            src="../img/FFF-Banner-Transparent.png"
+            alt="site-logo"
+            style="max-height: 125px"
+          />
+        </a>
+      </div>
 
-    <div class="navbar-menu" id="nav-links">
-      <div class="navbar-end">
-        <router-link v-bind:to="{ name: 'login' }" class="navbar-item">Login</router-link>
+      <div class="navbar-menu is-active" id="nav-links">
+        <div class="navbar-end">
+          <div class="buttons">
+            <router-link
+              v-bind:to="{ name: 'login' }"
+              class="button is-size-5 is-primary navbar-item"
+              >Login</router-link
+            >
+            <router-link
+              v-bind:to="{ name: 'register' }"
+              class="button is-size-5 is-success navbar-item"
+              >Create Account</router-link
+            >
+          </div>
         </div>
+      </div>
+    </nav>
+
+    <div class="section has-text-left">
+      <div class="dropdown is-hoverable">
+        <div class="dropdown-trigger">
+          <button
+            class="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu4"
+          >
+            <span>More</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
         </div>
-  </nav>
+        <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+          <div class="dropdown-content">
+            <a href="#" class="dropdown-item"> Overview</a>
+            <a href="#" class="dropdown-item"> Find a Pet</a>
+            <a href="#" class="dropdown-item"> Adopt a Pet</a>
+            <a href="#" class="dropdown-item"> About Us </a>
+            <a href="#" class="dropdown-item"> FAQ</a>
+            <hr class="dropdown-divider" />
+            <a href="#" class="dropdown-item"> More </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
