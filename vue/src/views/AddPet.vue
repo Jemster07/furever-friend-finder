e55ce375b08501cf81692fe4aabbcb8d4b093a81
Emelie Kerek <emelie.kerek@gmail.com>
2023-08-15 19:47:00 -0400

    Brought back BreedService and AddPet

--- vue/src/views/AddPet.vue
+++ vue/src/views/AddPet.vue
@@ -5,9 +5,13 @@
         Home
       </router-link>
 
-    <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="button is-light mx-3">
-      Logout
-    </router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        class="button is-light mx-3"
+      >
+        Logout
+      </router-link>
     </div>
 
     <div class="has-text-right py-4 px-4">
@@ -15,6 +19,7 @@
         Home
       </router-link>
     </div>
+
     <h1 class="has-text-centered is-size-1 py-3">Add a Pet</h1>
     <p class="has-text-centered py-3">
       Please fill out the required fields below
@@ -23,7 +28,9 @@
     <div class="py-3"></div>
   <div id="wholeform">
     <form id='formbox' @submit.prevent="newpet" class="has-text-centered">
+
       <div class="form-input-group">
+        <hr/>
         <label for="petname">Pet Name</label>
         <input id="petname" type="text" v-model="newpet.name" />
       </div>
@@ -52,17 +59,28 @@
       </div>
       <div class="form-input-group">
         <label for="petage">Pet Age</label>
-        <input id="petage" type="number" v-model="newpet.age" />
+        <select id="petage">
+          <option value="baby">Baby</option>
+          <option value="young">Young</option>
+          <option value="adult">Adult</option>
+          <option value="senior">Senior</option>
+        </select>
+        <input id="petage" type="hidden" v-model="newpet.age" />
       </div>
       <div class="form-input-group">
         <p>Please enter a short description for your pet.</p>
         <input id="petdesc" type="textarea" v-model="newpet.description" />
       </div>
+      <hr/>
     </form>
+    </div>
   </div>
 </template>
 
 <script>
+
+import BreedsService from '../services/BreedsService.js';
+
 export default {
   name: "addpet",
   data() {
@@ -74,9 +92,34 @@ export default {
         age: "",
         name: "",
         description: "",
-      },
-    };
+
+        },
+        breeds: [],
+    }
   },
-};
+  created() {
+        this.breeds = BreedsService.getBreedOfDogs();
+    }
+  
+}
 </script>
 
+<style>
+#addpet
+{
+  background-color:lightgreen
+}
+#wholeform {
+  display:flex;
+  height: 50vh;
+  justify-content: center;
+  justify-items: center;
+}
+#formbox{
+  background-color: rgb(196, 255, 201);
+  padding: 15px;
+  border:black solid 2px;
+  border-radius: 10px;
+  margin: 10px;
+}
+</style>
