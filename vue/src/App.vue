f329498a945d92e38623fc1ee13e138c964bf567
Riley Black <rstoneblack@gmail.com>
2023-08-15 10:35:27 -0400

    More Front End. Add Pet Table added, Login/Logout buttons in progress

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    <router-view />
-  </div>
+<div>
+  <router-view />
+</div>
 </template>
