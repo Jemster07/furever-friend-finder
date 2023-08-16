35db63381705cd1397f53be2d56b90e0f57271a3
Riley Black <rstoneblack@gmail.com>
2023-08-16 13:05:20 -0400

    Styling complete, beginning PetCard/UserCard components

--- vue/src/views/FriendDirectory.vue
+++ vue/src/views/FriendDirectory.vue
@@ -1,22 +1,77 @@
 <template>
   <div id="main-page">
-    <div id="header">
-      <div id="page-title" class="has-text-left is-size-2">All Friends</div>
-         
-      <div class="has-text-right py-4 px-4">
-        <router-link :to="{ name: 'home' }" class="button is-light">
-          Home
-        </router-link>
 
-        <router-link
-          v-bind:to="{ name: 'logout' }"
-          v-if="$store.state.token != ''"
-          class="button is-light mx-3"
-        >
-          Logout
-        </router-link>
+    <nav id="navigator">
+      <a>
+        <img
+          id="banner"
+          src="../img/FFF-Banner-Transparent.png"
+          alt="site-logo"
+          style="max-height: 150px"
+        />
+      </a>
+      <a id="tab-bar">
+        <ul id="tabs">
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'home' }"
+              style="color: black"
+              >HOME</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'searchpet' }"
+              style="color: black"
+              >SEARCH PETS</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'petdirectory' }"
+              style="color: black"
+              >VIEW PETS</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link v-bind:to="{ name: 'addpet' }" style="color: black"
+              >ADD NEW PET</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'frienddirectory' }"
+              style="color: black"
+              >VIEW FRIENDS</router-link
+            >
+          </li>
+        </ul>
+      </a>
+
+      <div>
+        <div id="buttons">
+          <router-link
+            v-bind:to="{ name: 'login' }"
+            class="button is-size-6 is-primary navbar-item my-3"
+            v-if="$store.state.token == ''"
+            >Login</router-link
+          >
+          <router-link
+            v-bind:to="{ name: 'register' }"
+            class="button is-size-6 is-success navbar-item my-3"
+            v-if="$store.state.token == ''"
+            >Create Account</router-link
+          >
+          <router-link
+            id="logout"
+            v-bind:to="{ name: 'logout' }"
+            class="button is-size-6 is-light"
+            v-if="$store.state.token != ''"
+            >Logout</router-link
+          >
+        </div>
       </div>
-    </div>
+    </nav>
 
     <div id="friend-list">
       <div>
@@ -63,5 +118,34 @@ export default {
   height: 10vh;
   border-bottom: 1px solid gray;
 }
+  #navigator {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid gray;
+  background-color: white;
+}
+#banner {
+  padding-left: 25px;
+}
+#buttons {
+  padding-right: 25px;
+}
+#tab {
+  font-size: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  border-left: 1px solid lightgreen;
+  border-right: 1px solid lightgreen;
+  border-radius: 2px;
+}
+#tabs {
+  display: flex;
+}
+
 </style>
 
