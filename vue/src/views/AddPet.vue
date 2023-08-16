72c177dd76507065377b27771fdfbecffeab7050
keith duncan <duncanclanof4@gmail.com>
2023-08-16 13:04:20 -0400

    this has the add a pet

--- vue/src/views/AddPet.vue
+++ vue/src/views/AddPet.vue
@@ -1,50 +1,94 @@
 <template>
-  <div id="addpet">
-    <div class="has-text-right py-4 px-4">
-      <router-link v-bind:to="{ name: 'home' }" class="button is-light">
-        Home
-      </router-link>
+  <div id="main-page" class="has-text-centered">
+    <nav id="navigator">
+      <a>
+        <img
+          id="banner"
+          src="../img/FFF-Banner-Transparent.png"
+          alt="site-logo"
+          style="max-height: 150px"
+        />
+      </a>
+      <a id="tab-bar">
+        <ul id="tabs">
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'searchpet' }"
+              style="color: black"
+              >SEARCH PETS</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'petdirectory' }"
+              style="color: black"
+              >VIEW PETS</router-link
+            >
+          </li>
+          <li id="tab">
+            <router-link
+              v-bind:to="{ name: 'frienddirectory' }"
+              style="color: black"
+              >VIEW FRIENDS</router-link
+            >
+          </li>
+        </ul>
+      </a>
 
-      <router-link
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != ''"
-        class="button is-light mx-3"
-      >
-        Logout
-      </router-link>
-    </div>
+      <div>
+        <div id="buttons">
+          <router-link
+            v-bind:to="{ name: 'login' }"
+            class="button is-size-6 is-primary navbar-item my-3"
+            v-if="$store.state.token == ''"
+            >Login</router-link
+          >
+          <router-link
+            v-bind:to="{ name: 'register' }"
+            class="button is-size-6 is-success navbar-item my-3"
+            v-if="$store.state.token == ''"
+            >Create Account</router-link
+          >
+          <router-link
+            id="logout"
+            v-bind:to="{ name: 'logout' }"
+            class="button is-size-6 is-light"
+            v-if="$store.state.token != ''"
+            >Logout</router-link
+          >
+        </div>
+      </div>
+    </nav>
 
-    <div class="has-text-right py-4 px-4">
-      <router-link :to="{ name: 'home' }" class="button is-light">
-        Home
-      </router-link>
-    </div>
 
-    <h1 class="has-text-centered is-size-1 py-3">Add a Pet</h1>
-    <p class="has-text-centered py-3">
-      Please fill out the required fields below
-    </p>
+
     <p />
     <div class="py-3"></div>
+    <div>
+      <h1>Add A Pet</h1>
+    </div>
   <div id="wholeform">
     <form id='formbox' @submit.prevent="newpet" class="has-text-centered">
 
-      <div class="form-input-group">
+      <div class="form-input-group">    
+        <p class="has-text-centered py-3">
+      Please fill out the required fields below
+    </p>
         <hr/>
         <label for="petname">Pet Name</label>
         <input id="petname" type="text" v-model="newpet.name" />
       </div>
       <div class="form-input-group">
         <label for="pettype">Pet Type </label>
-        <select for="pettype">
+        <select for="pettype" v-on:change="changeToOtherBreeds(newpet.type)" v-model="newpet.type">
           <option value="dog">Dog</option>
           <option value="cat">Cat</option>
           <option value="rabbit">Rabbit</option>
-          <option value="Horse">Horse</option>
+          <option value="horse">Horse</option>
           <option value="bird">Bird</option>
           <option value="other">Other</option>
         </select>
-        <input id="pettype" type="hidden" v-model="newpet.type" />
+        <input id="pettype" type="hidden"/>
       </div>
       <div class="form-input-group">
         <label for="petspecies">Pet Breed</label>
@@ -60,6 +104,7 @@
       <div class="form-input-group">
         <label for="petage">Pet Age</label>
         <select id="petage">
+          <option value=""></option>
           <option value="baby">Baby</option>
           <option value="young">Young</option>
           <option value="adult">Adult</option>
@@ -72,6 +117,10 @@
         <input id="petdesc" type="textarea" v-model="newpet.description" />
       </div>
       <hr/>
+      <button class="button is-success my-4" type="submit">
+        Add Pet
+      </button>
+
     </form>
     </div>
   </div>
@@ -86,7 +135,7 @@ export default {
   data() {
     return {
       newpet: {
-        type: "",
+        type: "Please select type",
         breed: "",
         color: "",
         age: "",
@@ -95,23 +144,55 @@ export default {
 
         },
         breeds: [],
+        newBreed: "",
     }
   },
   created() {
         this.breeds = BreedsService.getBreedOfDogs();
+  },
+  methods: {
+    changeToOtherBreeds(event){
+
+      if (event == "dog")
+      {
+        this.breeds = BreedsService.getBreedOfDogs();
+      }
+      else if (event == "cat")
+      {
+        this.breeds = BreedsService.getBreedsofCats();
+      }
+      else if (event == "rabbit")
+      {
+        this.breeds = BreedsService.getBreedsofRabbits();
+      }
+      else if (event == "horse")
+      {
+        this.breeds = BreedsService.getBreedsofHorse();
+      }
+      else if (event == "bird")
+      {
+        this.breeds = BreedsService.getBreedsofBirds();
+      }
+      else
+      {
+        this.breeds = BreedsService.getBreedsofOthers();
+      }
     }
+  }
+};
   
-}
+
 </script>
 
-<style>
+<style scoped>
+
 #addpet
 {
   background-color:lightgreen
 }
 #wholeform {
   display:flex;
-  height: 50vh;
+  height: 75vh;
   justify-content: center;
   justify-items: center;
 }
@@ -121,5 +202,82 @@ export default {
   border:black solid 2px;
   border-radius: 10px;
   margin: 10px;
+}
+#footer{
+  color: darkgrey;
+}
+#card {
+  display: inline-block;
+  padding-top: 1rem;
+  padding-left: 1vw;
+  padding-right: 1vw;
+  margin-left: 1vw;
+  margin-right: 1vw;
+  width: 22vw;
+  height: 60vh;
+  margin-bottom: 4rem;
+  margin-top: 3rem;
+}
+#lower-page {
+  padding-top: 25px;
+  background-color: rgb(rgba(11, 243, 135, 0.575), green, blue);
+}
+#banner {
+  padding-left: 25px;
+}
+#buttons {
+  padding-right: 25px;
+}
+#tab {
+  font-size: 20px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  border-left: 1px solid lightgreen;
+  border-right: 1px solid lightgreen;
+  border-radius: 2px;
+}
+#tabs {
+  display: flex;
+}
+#navigator {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid gray;
+  background-color: white;
+}
+#welcome-box {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
+}
+#welcome-message {
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.281);
+  color: rgb(10, 82, 58);
+
+  width: 100vw;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+}
+#main-page {
+  background-color: lightgreen;
+  height: 160vh;
+}
+#message-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin-left: 5rem;
+  margin-right: 5rem;
+  width: 90vw;
+  height: 40vh;
+  padding-bottom: 10rem;
+  margin-bottom: 3rem;
 }
 </style>
