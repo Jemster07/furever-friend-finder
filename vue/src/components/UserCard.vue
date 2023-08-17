a3751ee758b941aa7085ae1442e7f182dd7b6b7e
Emelie Kerek <emelie.kerek@gmail.com>
2023-08-17 09:52:47 -0400

    WIP

--- vue/src/components/UserCard.vue
+++ vue/src/components/UserCard.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="card" id="card">
         <div class="card-image">
-          <figure class="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder img"
-            />
-          </figure>
+          
         </div>
         <div class="card-content">
           <div class="media">
             <div class="media-left">
-              <figure class="image is-48x48">
-                <img
-                  src="https://bulma.io/images/placeholders/96x96.png"
-                  alt="Placeholder img"
-                />
-              </figure>
+              
             </div>
             <div class="media-content has-text-left">
               <p class="title is-4">{{displayUser.username}}</p>
@@ -27,41 +17,26 @@
           <div class="content">
             {{displayUser.address.street}}
           </div>
+          <div class="content">
+            {{displayUser.address.city}}
+          </div>
+          <div class="content">
+            {{displayUser.address.state}}
+          </div>
+          <div class="content">
+            {{displayUser.address.zip}}
+          </div>
+
         </div>
       </div>
 </template>
 
 <script>
-// import UsersService from '../services/UsersService.js';
 
 export default {
     name: 'user-card',
     props: ['displayUser'],
-    data() {
-      return {
-        // tempUser: {
-        //   userId: 0,
-        //   username: "",
-        //   role: "",
-        //   address: {
-        //     addressId: 0,
-        //     street: "",
-        //     city: "",
-        //     state: "",
-        //     zip: 0,
-        //   },
-        //   applicationStatus: "",
-        //   isInactive: false,
-        //   email: "",
-        //   isAdopter: false
-        // }
-      }
-    },
-  //   created() {
-  //       UsersService.GetUser(this.users.displayUser.username).then(response => {
-  //       this.displayUser = response.data;
-  //   });
-  // }
+
 }
 </script>
 
--- vue/src/views/AdminPage.vue
+++ vue/src/views/AdminPage.vue
@@ -2,12 +2,17 @@
   <div> 
       This is where you go when you have the "admin" role and would like to see a list of all pending "friend" applications
       <p> This will take the account info from "user" roles applying for a "friend" role </p> 
+              
+    <button class="button is-success my-4" type="submit">
+      <router-link v-bind:to="{ name: 'frienddirectory' }" style="color: white">Pending Users</router-link>
+    </button>
+
   </div>
 </template>
 
 <script>
 export default {
-    name: 'pendingapp'
+    name: 'adminpage'
 }
 </script>
 
